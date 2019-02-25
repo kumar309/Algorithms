323d0a82925a3835b9fc47468d92998898f78338
Kumar <kumar.keswani@gmail.com>
2019-02-24 19:42:33 -0500

    alternate method.

--- com/kumar/leetcode/MaxStockProfit.java
+++ com/kumar/leetcode/MaxStockProfit.java
@@ -1,13 +1,18 @@
 package com.kumar.leetcode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MaxStockProfit {
 
     public static void main(String[] args) {
         MaxStockProfit profit = new MaxStockProfit();
         int[] arr = {7,1,5,3,6,4};
+        System.out.println("original:" + Arrays.toString(arr));
         //int[] arr = {7,6,4,3,1};
         int result = profit.maxProfit(arr);
         System.out.println("max profit:{}"+result);
+        //System.out.println("max profit:{}"+profit.maxprofit2(arr))
     }
 
     public int maxProfit(int[] prices) {
@@ -26,5 +31,23 @@ public class MaxStockProfit {
             }
         }
         return res;
+    }
+
+    public int maxprofit2(int[] prices){
+        int profit = 0;
+        int min = 0;
+
+        if(prices==null|| prices.length<=1)
+            return profit;
+
+        for(int i=1;i<prices.length;i++){
+            if(prices[i] < min){
+                min = prices[i];
+            }else{
+                profit = Math.max(profit,prices[i]-min);
+            }
+        }
+
+        return profit;
     }
 }
