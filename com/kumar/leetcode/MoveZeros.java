1cda61350efe536352998df6cf892f0bf8772cd5
kumar309 <kumar.keswani@gmail.com>
2020-08-11 00:09:31 -0400

    some code solutions

--- com/kumar/leetcode/MoveZeros.java
+++ com/kumar/leetcode/MoveZeros.java
@@ -3,47 +3,43 @@ package com.kumar.leetcode;
 public class MoveZeros {
     public static void main(String[] args) {
         MoveZeros moveZeros = new MoveZeros();
-        int[] arr= {0,1,5,0,3,0,12};
+        int[] arr = {0, 1, 5, 0, 3, 0, 12};
         //System.out.println(moveZeros.moveZeroes(arr).toString());
-        //int[] result = moveZeros.moveZeroes(arr);
-        int[] result = moveZeros.moveZeros(arr);
-        for(int i:result){
+        int[] result = moveZeros.moveZeroes(arr);
+        //int[] result = moveZeros.moveZeros(arr);
+        for (int i : result) {
             System.out.println(i);
         }
     }
 
     public int[] moveZeroes(int[] nums) {
-        int pos =0;
-        for(int i=0; i< nums.length;i++){
-            if(nums[i] != 0){
-                System.out.println("pos:"+ pos);
-                nums[pos++] = nums[i];
-                System.out.println("pos_incr:"+pos + " nums[i]:"+nums[i]);
+        int pos = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[pos] = nums[i];
+                pos++;
             }
         }
         //at this point pos is incremented till the index which has last non 0 value.so continue after that put 0 till length of array.
-        for(; pos < nums.length; pos++){
-            nums[pos] =0;
+        for (; pos < nums.length; pos++) {
+            nums[pos] = 0;
         }
-        return  nums;
+        return nums;
     }
 
-
-    public int[] moveZeros(int[] nums){
-        int count=0;
+    public int[] moveZeros(int[] nums) {
+        int count = 0;
         int len = nums.length;
 
-        for(int i=0; i<nums.length;i++){
-            if(nums[i]!=0){
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
                 nums[count++] = nums[i];
             }
         }
 
-        while(count < len){
+        while (count < len) {
             nums[count++] = 0;
         }
-
-
         return nums;
     }
 }
--- com/kumar/leetcode/RemoveDuplicateinArray.java
+++ com/kumar/leetcode/RemoveDuplicateinArray.java
@@ -1,12 +1,17 @@
 package com.kumar.leetcode;
 
+import java.util.Arrays;
+
 class RemoveDuplicateinArray {
     public static void main(String[] args) {
-        int[] arr = {1,2,4,4,5,5,6,7};
+        int[] arr = {1, 2, 4, 4, 5, 5, 6, 7};
         RemoveDuplicateinArray instance = new RemoveDuplicateinArray();
         //System.out.println(instance.removeDuplicates(arr));
-        System.out.println(instance.removeDuplicates2(arr));
-    }
+        //System.out.println(instance.removeDuplicates2(arr));
+        //System.out.println(instance.removeDupsArray(arr));
+        testArray(arr);
+}
+
     public int removeDuplicates(int[] nums) {
         int i = 0;
 
@@ -24,18 +29,54 @@ class RemoveDuplicateinArray {
         if (nums.length <= 1) return nums.length;
         int i = 0;
         //int j = 1;
-        for (int j=1; j < nums.length; ) {
-            if (nums[i] == nums[j]){
+        for (int j = 1; j < nums.length; ) {
+            if (nums[i] == nums[j]) {
                 j++;
-            }
-            else{
+            } else {
                 nums[++i] = nums[j++];
-                System.out.println("i:"+i + " j:" + j);
+                System.out.println("i:" + i + " j:" + j);
             }
         }
         return ++i;
     }
 
+    public int removeDupsArray(int[] arr) {
+        int n = arr.length;
+        if (n == 0 || n == 1)
+            return n;
+
+        // To store index of next unique element
+        int j = 0;
+
+        // Doing same as done in Method 1
+        // Just maintaining another updated index i.e. j
+        for (int i = 0; i < n-1; i++)
+            if (arr[i] != arr[i+1])
+                arr[j++] = arr[i];
+
+        arr[j++] = arr[n-1];
+
+        for(int k =0; k<j-1; k++){
+            System.out.println(arr[k]);
+        }
+
+        return j;
+    }
+
+    public  static void testArray(int[] arr){
+        if(arr==null || arr.length==0)
+            return;
+
+        for(int i=0; i< arr.length-1;i++){
+            System.out.println("arr[i++]:" + arr[i++]);
+            System.out.println("i:" + i);
+            System.out.println("***************");
+            System.out.println("arr[++i]:" + arr[++i]);
+            System.out.println("i:" + i);
+        }
+    }
+
 
 }
+
 
