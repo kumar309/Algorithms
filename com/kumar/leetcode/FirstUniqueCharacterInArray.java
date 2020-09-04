93c3af2a388e268af88e65d9216ad8ff9a0e7af0
kumar309 <kumar.keswani@gmail.com>
2020-09-04 00:19:15 -0400

    return first unique characte or index position

--- com/kumar/leetcode/FirstUniqueCharacterInArray.java
+++ com/kumar/leetcode/FirstUniqueCharacterInArray.java
@@ -5,27 +5,56 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 class FirstUniqueCharacterInArray {
-    public int firstUniqChar(String s) {
-        HashMap<Character,Integer> myMap = new LinkedHashMap<>();
-        char[] c = s.toCharArray();
-        int count=0;
-        for(int i=0;i<c.length;i++){
-            //Character c = s.char(i);
-            if(!myMap.containsKey(c[i])){
-                myMap.put(c[i],1);
-            }else{
-                count = myMap.get(c[i]);
-                myMap.put(c[i],count+1);
-            }
-        }
-
-        for(Character character: myMap.keySet() ){
-            count++;
-            if(myMap.containsValue(1)){
-
-            }
-
-        }
-        return count;
+    public static void main(String[] args) {
+        FirstUniqueCharacterInArray fuca = new FirstUniqueCharacterInArray();
+        System.out.println(fuca.firstUniqueChar("loveleetcode"));
+        System.out.println(fuca.firstUniqueCharIndex("loveleetcode"));
     }
+
+    public Character firstUniqueChar(String s) {
+        Character result = null;
+        if (s == null || s.length() == 0)
+            return null;
+
+        Map<Character, Integer> freqMap = new LinkedHashMap<>(s.length() - 1);
+        for (char c : s.toCharArray()) {
+            if (!freqMap.containsKey(c)) {
+                freqMap.put(c, 1);
+            } else {
+                freqMap.put(c, -1);
+            }
+        }
+
+        for (Map.Entry<Character, Integer> entry : freqMap.entrySet()) {
+            if (entry.getValue() != -1) {
+                result = entry.getKey();
+                return result;
+            }
+        }
+        return result;
+    }
+
+    public int firstUniqueCharIndex(String s) {
+        Character result = null;
+        if (s == null || s.length() == 0)
+            return -1;
+
+        char[] c = s.toCharArray();
+        Map<Character, Integer> freqMap = new LinkedHashMap<>(s.length() - 1);
+        for (int i = 0; i < s.length(); i++) {
+            if (!freqMap.containsKey(c[i])) {
+                freqMap.put(c[i], i);
+            } else {
+                freqMap.put(c[i], -1);
+            }
+        }
+
+        for (Map.Entry<Character, Integer> entry : freqMap.entrySet()) {
+            if (entry.getValue() != -1) {
+                return entry.getValue();
+            }
+        }
+        return -1;
+    }
+
 }
