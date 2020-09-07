943982a38a00ef39238ba0043b9e11953de7c591
kumar309 <kumar.keswani@gmail.com>
2020-09-07 13:49:34 -0400

    another way using ascii value of char

--- com/kumar/leetcode/FirstUniqueCharacterInArray.java
+++ com/kumar/leetcode/FirstUniqueCharacterInArray.java
@@ -9,6 +9,7 @@ class FirstUniqueCharacterInArray {
         FirstUniqueCharacterInArray fuca = new FirstUniqueCharacterInArray();
         System.out.println(fuca.firstUniqueChar("loveleetcode"));
         System.out.println(fuca.firstUniqueCharIndex("loveleetcode"));
+        System.out.println(fuca.firstUniqCharIndex("loveleetcode"));
     }
 
     public Character firstUniqueChar(String s) {
@@ -57,4 +58,20 @@ class FirstUniqueCharacterInArray {
         return -1;
     }
 
+    public int firstUniqCharIndex(String s) {
+        if (s == null || s.isEmpty())
+            return -1;
+
+        int[] map = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            map[s.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (map[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
